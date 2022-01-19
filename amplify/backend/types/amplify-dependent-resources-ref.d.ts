59c27a3d00df5ac141c0327f5620f14ac45b1c69
Sifei <sifei@users.noreply.github.com>
2022-01-19 04:11:46 -0500

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "amplifyapp020a42e5": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./App.css";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig)
 
 function App() {
+  const [username,setUsername] = useState("")
+  const funcc = async () => {
+    let user = await Auth.currentAuthenticatedUser();
+    console.log(user)
+    const { username } = user;
+    setUsername(username)
+  }
+  funcc()
   return (
     <div className="App">
       <header className="App-header">
+        <div>
+          <h3 id="userName"> Welcome {username} </h3>
+        </div>
+        <p></p>
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>
+        <a
+          className="App-link"
+          href="https://tsui-wakeupsafe.research.chop.edu"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Wakeup Safe
+        </a>
+
+        <AmplifySignOut />
       </header>
+
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
